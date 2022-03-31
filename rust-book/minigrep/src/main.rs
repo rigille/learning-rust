6bb6525c16ac7e779aeb12c9d9ac84a80b518c59
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2022-03-31 16:37:09 -0300

    split code and handle errors

--- rust-book/minigrep/src/main.rs
+++ rust-book/minigrep/src/main.rs
@@ -1,6 +1,46 @@
+use std::fs;
 use std::env;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("Inside {}", config.filename);
+
+    let res = run(config);
+    if let Err(e) = res {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            Err("not enough arguments")
+        } else {
+            let query = args[1].clone();
+            let filename = args[2].clone();
+            Ok(Config { query, filename })
+        }
+    }
 }
